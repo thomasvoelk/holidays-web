4b6c3c10b6b8beb07a521640c920c0970453267f
Thomas Voelk <thomas.voelk@gmail.com>
2012-02-13 18:40:46 +0100

    HolidayCalculator is not injected anymore

--- java/src/org/voelk/holidays/web/application/ApplicationModule.java
+++ java/src/org/voelk/holidays/web/application/ApplicationModule.java
@@ -1,14 +1,12 @@
 package org.voelk.holidays.web.application;
 
 import com.google.inject.*;
-import org.voelk.holidays.application.*;
 import org.voelk.holidays.gateways.*;
 import org.voelk.holidays.web.gateways.*;
 
 public class ApplicationModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(HolidayCalculator.class).to(JollydayHolidayCalculator.class);
         bind(UserGateway.class).to(GaeDatastoreUserGateway.class);
     }
 }
